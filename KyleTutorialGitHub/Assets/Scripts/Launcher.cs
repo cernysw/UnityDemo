208bd59ec9109770330f5a47343cda5b94dca226
David Cerny <David.Cerny@solarwinds.com>
2018-08-06 19:39:51 +0200

    connectiing

---
+++ KyleTutorialGitHub/Assets/Scripts/Launcher.cs
@@ -0,0 +1,40 @@
+﻿using Photon;
+using UnityEngine;
+
+namespace A.Kyle
+{
+    public class Launcher : PunBehaviour
+    {
+        string _gameVersion = "1";
+        private void Awake()
+        {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            if (PhotonNetwork.connected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("OnConnectedToMaster");
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            Debug.Log("OnDisconnectedFromPhoton");
+        }
+
+
+    }
+}
