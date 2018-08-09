ed10e851688bcc2a325f373a0cc2b597109f0897
David Cerny <David.Cerny@solarwinds.com>
2018-08-09 21:20:33 +0200

    controller fix

--- UnityIsland/Assets/Scripts/PlaneController.cs
+++ UnityIsland/Assets/Scripts/PlaneController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    public float m_speed = 5;
-    public float m_rotationSpeed = 5;
+    public float m_speed = 59;
+    public float m_rotationSpeed = 59;
 
     void Start ()
     {
@@ -17,14 +17,16 @@ public class PlaneController : MonoBehaviour
     {
         var t = this.gameObject.transform;
 
+
         var rotation = new Vector3();
         rotation.x = Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime;
         rotation.y = Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime;
-        t.Rotate(rotation);
         //rotation.z = Input.GetAxis("Axe3") * _rotationSpeed * Time.deltaTime;
+        t.Rotate(rotation);
 
-        //t.Rotate(t.right, Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime);
-        //t.Rotate(t.up, Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime);
+
+        //t.Rotate(t.right, Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime, Space.Self);
+        //t.Rotate(t.up, Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime, Space.Self);
         t.position += t.forward * m_speed * Time.deltaTime;
 	}
 }
