e6a98b5b66499ee5c50b36bc15a938327ab8eb34
David Cerny <David.Cerny@solarwinds.com>
2018-08-10 10:54:56 +0200

    throttle

--- UnityIsland/Assets/Scripts/PlaneController.cs
+++ UnityIsland/Assets/Scripts/PlaneController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    public float m_speed = 59;
-    public float m_rotationSpeed = 59;
+    public float m_speed = 50;
+    public float m_rotationSpeed = 40;
+    public float m_acceleration = 5;
 
     void Start ()
     {
@@ -18,15 +19,15 @@ public class PlaneController : MonoBehaviour
         var t = this.gameObject.transform;
 
 
-        //var rotation = new Vector3();
-        //rotation.x = Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime;
-        //rotation.y = Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime;
+        var rotation = new Vector3();
+        rotation.x = Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime;
+        rotation.y = Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime;
         //rotation.z = Input.GetAxis("Axe3") * _rotationSpeed * Time.deltaTime;
-        //t.Rotate(rotation);
+        t.Rotate(rotation);
 
 
-        t.Rotate(t.right, Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime, Space.World);
-        t.Rotate(t.up, Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime, Space.World);
+        m_speed += Input.GetAxis("Throttle") * m_acceleration * Time.deltaTime;
+        m_speed = m_speed < 0 ? 0 : m_speed;
         t.position += t.forward * m_speed * Time.deltaTime;
 	}
 }
