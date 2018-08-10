3cb638e1f67643a4bcbaff7e7100c4ae18172463
David Cerny <David.Cerny@solarwinds.com>
2018-08-10 11:56:55 +0200

    water + hold direction

--- UnityIsland/Assets/Scripts/PlaneController.cs
+++ UnityIsland/Assets/Scripts/PlaneController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class PlaneController : MonoBehaviour
@@ -7,6 +8,8 @@ public class PlaneController : MonoBehaviour
     public float m_speed = 50;
     public float m_rotationSpeed = 40;
     public float m_acceleration = 5;
+    public float m_holdHorizontal = 0;
+    public float m_holdVertical = 0;
 
     void Start ()
     {
@@ -18,10 +21,19 @@ public class PlaneController : MonoBehaviour
     {
         var t = this.gameObject.transform;
 
+        if (Input.GetKeyDown(KeyCode.Keypad6)) m_holdHorizontal = +1;
+        if (Input.GetKeyDown(KeyCode.Keypad4)) m_holdHorizontal = -1;
+        if (Input.GetKeyDown(KeyCode.Keypad8)) m_holdVertical = +1;
+        if (Input.GetKeyDown(KeyCode.Keypad2)) m_holdVertical = -1;
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            m_holdVertical = m_holdHorizontal = 0;
+        }
+
 
         var rotation = new Vector3();
-        rotation.x = Input.GetAxis("Vertical") * m_rotationSpeed * Time.deltaTime;
-        rotation.y = Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime;
+        rotation.x = (m_holdVertical + Input.GetAxis("Vertical")) * m_rotationSpeed * Time.deltaTime;
+        rotation.y = (m_holdHorizontal + Input.GetAxis("Horizontal")) * m_rotationSpeed * Time.deltaTime;
         //rotation.z = Input.GetAxis("Axe3") * _rotationSpeed * Time.deltaTime;
         t.Rotate(rotation);
 
