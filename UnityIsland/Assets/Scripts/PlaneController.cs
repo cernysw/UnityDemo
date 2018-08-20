d0a9c01c25e8a4810d08c767bc1499525e815e73
David Cerny <David.Cerny@solarwinds.com>
2018-08-20 12:23:41 +0200

    OnSerialize + RPC

--- UnityIsland/Assets/Scripts/PlaneController.cs
+++ UnityIsland/Assets/Scripts/PlaneController.cs
@@ -3,21 +3,21 @@
 namespace UnityIsland
 {
 
-    public class PlaneController : Photon.MonoBehaviour
+    public class PlaneController : Photon.MonoBehaviour, IPunObservable
     {
         public GameObject m_projectilePrefab;
-        //private GameObject m_projectileSpawnPoint;
         public float m_speed = 50;
         public float m_projectileSpeed = 500;
         public float m_rotationSpeed = 40;
         public float m_acceleration = 50;
         public float m_holdHorizontal = 0;
         public float m_holdVertical = 0;
+        public int  m_ammo = 1000;
+        private bool m_isFiring = false;
 
         private void Awake()
         {
             //m_projectileSpawnPoint = this.gameObject..transform.FindChild("ProjectileSpawnPoint");
-            
         }
 
         void Update()
@@ -25,14 +25,20 @@ namespace UnityIsland
             ControlPlane();
         }
 
-        public void FireProjectile()
+        [PunRPC]
+        private void Fire()
         {
+            if (m_ammo <= 0)
+            {
+                Debug.LogWarning("Cannot fire no ammo");
+                return;
+            }
+            m_ammo--;
             var spawnTransform = this.gameObject.transform.Find("ProjectileSpawnPoint");
             var projectile = Instantiate(m_projectilePrefab);
             projectile.transform.position = spawnTransform.position;
             projectile.transform.rotation = spawnTransform.rotation;
             projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * (m_speed + m_projectileSpeed);
-
             GameObject.Destroy(projectile, 3);
         }
 
@@ -42,10 +48,12 @@ namespace UnityIsland
             {
                 var t = this.gameObject.transform;
 
-                if (Input.GetButton("Fire1"))
+                if (Input.GetButtonDown("Fire1"))
                 {
-                    this.FireProjectile();
+                    PhotonView.Get(this).RPC("Fire", PhotonTargets.AllViaServer);
                 }
+
+                m_isFiring = Input.GetButton("Fire3");
 
                 if (Input.GetKeyDown(KeyCode.Keypad6)) m_holdHorizontal = +1;
                 if (Input.GetKeyDown(KeyCode.Keypad4)) m_holdHorizontal = -1;
@@ -55,7 +63,6 @@ namespace UnityIsland
                 {
                     m_holdVertical = m_holdHorizontal = 0;
                 }
-
 
                 var rotation = new Vector3();
                 rotation.x = (m_holdVertical + Input.GetAxis("Vertical")) * m_rotationSpeed * Time.deltaTime;
@@ -71,6 +78,25 @@ namespace UnityIsland
 
                 this.gameObject.GetComponent<PhotonTransformView>().SetSynchronizedValues(speedVector, rotation.y / Time.deltaTime);
             }
+
+            if (m_isFiring)
+            {
+                Fire();
+            }
         }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.isWriting)
+            {
+                stream.SendNext(m_isFiring);
+            }
+            else
+            {
+                m_isFiring = (bool)stream.ReceiveNext();
+            }
+        }
+
+
     }
 }
