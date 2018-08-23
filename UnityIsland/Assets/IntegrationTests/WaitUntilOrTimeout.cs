f86d2d4daee984b6127e03045f4779dfda2e7ef3
David Cerny <David.Cerny@solarwinds.com>
2018-08-23 12:50:47 +0200

    GameManagertest refactoring

---
+++ UnityIsland/Assets/IntegrationTests/WaitUntilOrTimeout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityIsland
+{
+    public sealed class WaitUntilOrTimeout : CustomYieldInstruction
+    {
+        private readonly Func<bool> m_Predicate;
+        private readonly TimeSpan m_Timeout;
+        private readonly DateTime m_StartTime;
+
+        public WaitUntilOrTimeout(Func<bool> predicate, TimeSpan timeout)
+        {
+            m_Predicate = predicate;
+            m_Timeout = timeout;
+            m_StartTime = DateTime.Now;
+        }
+
+        public override bool keepWaiting
+        {
+            get { return !this.m_Predicate() || DateTime.Now - m_StartTime > m_Timeout; }
+        }
+    }
+}
